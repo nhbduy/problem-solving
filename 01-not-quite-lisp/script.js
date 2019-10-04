2935459a7e9ad4acf20f99255d0c48acae18e634
Duy Nguyen <duy.nguyen@christinas.vn>
2019-10-04 23:04:10 +0700

    Added array reduce solutions

--- 01-not-quite-lisp/script.js
+++ 01-not-quite-lisp/script.js
@@ -2,22 +2,30 @@ const fs = require('fs');
 
 fs.readFile('./input.txt', (err, data) => {
   if (err) console.log(err);
-  const input = data.toString();
+  const dataArray = data.toString().split('');
 
-  console.time('part1');
-  console.log(calculateFloor(input));
-  console.timeEnd('part1');
+  console.time('part1-1');
+  console.log(calculateFloor01(dataArray));
+  console.timeEnd('part1-1');
 
-  console.time('part2');
-  console.log(findPosition(input));
-  console.timeEnd('part2');
+  console.time('part1-2');
+  console.log(calculateFloor02(dataArray));
+  console.timeEnd('part1-2');
+
+  console.time('part2-1');
+  console.log(findPosition01(dataArray));
+  console.timeEnd('part2-1');
+
+  console.time('part2-2');
+  console.log(findPosition02(dataArray));
+  console.timeEnd('part2-2');
 });
 
 // '(' ---> go UP 1 floor
 // ')' ---> go DOWN 1 floor
 
 // Part 1: What floor does Santa end up on?
-const calculateFloor = data => {
+const calculateFloor01 = data => {
   let floor = 0;
   let position = 0;
   do {
@@ -30,8 +38,20 @@ const calculateFloor = data => {
   return floor;
 };
 
+const calculateFloor02 = data => {
+  const result = data.reduce((acc, curValue) => {
+    if (curValue === '(') {
+      return (acc += 1);
+    } else if (curValue === ')') {
+      return (acc -= 1);
+    }
+  }, 0);
+
+  return result;
+};
+
 // Part 2: When does Santa first enter the basement?
-const findPosition = data => {
+const findPosition01 = data => {
   let floor = 0;
   let position = 0;
 
@@ -43,4 +63,22 @@ const findPosition = data => {
   } while (floor !== -1);
 
   return position;
+};
+
+const findPosition02 = data => {
+  let acc = 0;
+  let counter = 0;
+  const result = data.some(curValue => {
+    if (curValue === '(') {
+      acc += 1;
+    } else if (curValue === ')') {
+      acc -= 1;
+    }
+
+    counter++;
+
+    return acc < 0;
+  });
+
+  return counter;
 };
